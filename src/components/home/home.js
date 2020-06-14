d0c8a685787b112d2e49673e43f3587b0974b53a
Brandon Cantello <bcantello@gmail.com>
2020-06-13 23:03:05 -0700

    updates search results to appear on new page

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,12 +1,10 @@
 import React from "react";
 import RepSearchForm from "../repSearchForm/repSearchForm";
-import ElectedOfficialsList from "../electedOfficials/electedOfficialsList";
 
 export default function Home() {
 	return (
 		<>
 			<RepSearchForm/>
-			<ElectedOfficialsList/>
 		</>
 	)
 };
