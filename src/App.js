65247d3991cdae4a358746fb7f6e9b33ad3e7d0a
Brandon Cantello <bcantello@gmail.com>
2020-06-15 23:03:00 -0700

    fixes deploy error

--- src/App.js
+++ src/App.js
@@ -7,15 +7,10 @@ import Footer from "./components/navigation/footer";
 require('dotenv').config();
 
 function App() {
-    const [zipcode, setZipcode] = useState('');
     const [officials, setOfficials] = useState(() => {
         const result = localStorage.getItem('electedOfficials');
         return result ? JSON.parse(result) : {}
     });
-
-    const handleZipcodeSubmit = (zip) => {
-        setZipcode(zip);
-    };
 
   return (
     <div className="App">
@@ -24,7 +19,6 @@ function App() {
         <div className={'content'}>
             <UniversalContext.Provider value={
                 {
-                    handleZipcodeSubmit,
                     officials,
                     setOfficials
                 }
