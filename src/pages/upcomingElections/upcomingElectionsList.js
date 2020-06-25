bbbd5b5da42daca23d89e2e06d3689918beff8ce
Brandon Cantello <bcantello@gmail.com>
2020-06-24 22:38:16 -0700

    completes upcomingElectionsList.js

--- src/pages/upcomingElections/upcomingElectionsList.js
+++ src/pages/upcomingElections/upcomingElectionsList.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, {useContext} from "react";
+import {UniversalContext} from "../../App";
+import UpcomingElectionsListItem from "./upcomingElectionsListItem";
 
 export default function UpcomingElectionsList() {
+	const universalContext = useContext(UniversalContext);
+	const electionsList = universalContext.electionInfo.elections;
+
+	if (sessionStorage.getItem('electionsList') === null) {
+		electionsList.shift();
+		sessionStorage.setItem('electionsList', JSON.stringify(electionsList));
+	}
+	console.log(JSON.parse(sessionStorage.getItem('electionsList')));
+
+	const electionsArr = JSON.parse(sessionStorage.getItem('electionsList')).map((e, index) => {
+		return (
+			<UpcomingElectionsListItem element={e} key={index}/>
+		);
+	});
+
 	return (
-		<div>
-			upcoming elections list
-		</div>
+		<div>{electionsArr}</div>
 	);
 };
