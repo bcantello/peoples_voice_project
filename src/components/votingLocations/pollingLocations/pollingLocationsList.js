aeb67c9b7f8591b5df9f1d675fc325bf88ba2566
Brandon Cantello <bcantello@gmail.com>
2020-07-22 17:14:55 -0700

    fixes crash when no polling locations are listed

--- src/components/votingLocations/pollingLocations/pollingLocationsList.js
+++ src/components/votingLocations/pollingLocations/pollingLocationsList.js
@@ -5,12 +5,15 @@ import PollingLocationsListItem from "./pollingLocationsListItem";
 export default function PollingLocationsList() {
 	const universalContext = useContext(UniversalContext);
 	const data = universalContext.pollingLocations.data;
+	let pollingLocationsArr = this;
 
-	const pollingLocationsArr = data.pollingLocations.map((e, index) => {
-		return (
-			<PollingLocationsListItem element={e} key={index}/>
-		);
-	});
+	if (data.pollingLocations !== undefined) {
+		pollingLocationsArr = data.pollingLocations.map((e, index) => {
+			return (
+				<PollingLocationsListItem element={e} key={index}/>
+			);
+		});
+	}
 
 	return (
 		<div>{pollingLocationsArr}</div>
