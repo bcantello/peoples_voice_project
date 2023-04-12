f3b23018723a3319bd25fef8c4304ffd86ea2721
Mikel <mikel362d@gmail.com>
2023-04-11 19:11:54 -0700

    adds loader component and logic to upcomingElectionsList and Home pages to avoid loading a page with a navbar but a blank body

---
+++ src/components/loader/loader.js
@@ -0,0 +1,6 @@
+import React from "react";
+import './loader.css'
+
+export default function Loading() {
+	return <div class="loader"></div>;
+}
--- src/pages/upcomingElections/upcomingElectionsList.js
+++ src/pages/upcomingElections/upcomingElectionsList.js
@@ -2,14 +2,23 @@ import React, {useContext} from "react";
 import {UniversalContext} from "../../App";
 import UpcomingElectionsListItem from "./upcomingElectionsListItem";
 import './upcomingElections.css';
+import Home from "../home/home";
 
 export default function UpcomingElectionsList() {
 	const universalContext = useContext(UniversalContext);
 	const electionsList = universalContext.electionInfo.elections;
 
 	if (sessionStorage.getItem('electionsList') === null) {
-		electionsList.shift();
-		sessionStorage.setItem('electionsList', JSON.stringify(electionsList));
+		if (electionsList !== undefined) {
+			electionsList.shift();
+			sessionStorage.setItem('electionsList', JSON.stringify(electionsList));
+		}
+		else {
+			const needs_loading = true;
+			return (
+				<Home needs_loading={needs_loading}/>
+			)
+		}
 	}
 
 	const electionsArr = JSON.parse(sessionStorage.getItem('electionsList')).map((e, index) => {
